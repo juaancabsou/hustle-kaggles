acb3b7722939ba5aeecac34b8ca3a5905abe89df
Juan Antonio <juaancabsou@gmail.com>
2023-09-14 09:03:07 +0200

    K_Spaceship - Filling missing values

--- spaceship/preprocessing/preproc_features.py
+++ spaceship/preprocessing/preproc_features.py
@@ -1,6 +1,32 @@
 from preprocessing.aux_feature_ages import assign_age_group
 
-def feature_engineering_iteration_one(df):
+def feature_engineering_it_1(df):
+    """
+    Perform the first iteration of feature engineering on the Titanic dataset.
+
+    This function adds several new features to the DataFrame based on age, billing, cabin, passenger ID, and group size.
+
+    Args:
+        df (DataFrame): The input DataFrame containing Titanic passenger data.
+
+    Returns:
+        DataFrame: The DataFrame with new features added.
+    """
+
+    # Create new features related to passenger ID and group size
+    df['fe_group_id'] = df['PassengerId'].apply(lambda x: int(str(x)[:4]))
+    df['fe_group_size'] = df.groupby('fe_group_id')['PassengerId'].transform('count')
+    df['fe_is_alone'] = df['fe_group_size'].apply(lambda x: 'alone' if x == 1 else 'not_alone')
+
+    # Fill missing values in the 'Name' column with 'Unknown Unknown'
+    df['Name'].fillna('Unknown Unknown', inplace=True)
+
+    # Create a 'Surname' column by extracting the last word from the 'Name' column
+    df['Surname'] = df['Name'].str.split().str[-1]
+
+    return df
+
+def feature_engineering_it_2(df):
     """
     Perform the first iteration of feature engineering on the Titanic dataset.
 
@@ -26,13 +52,5 @@ def feature_engineering_iteration_one(df):
     df[cols_cabin] = df['Cabin'].str.split('/', expand=True)
     for col in cols_cabin:
         df[col] = df[col].astype(str)
-
-    # Create new features related to passenger ID and group size
-    df['fe_group_id'] = df['PassengerId'].apply(lambda x: int(str(x)[:4]))
-    df['fe_group_size'] = df.groupby('fe_group_id')['PassengerId'].transform('count')
-    df['fe_is_alone'] = df['fe_group_size'].apply(lambda x: 'alone' if x == 1 else 'not_alone')
-
-    # Drop the temporary 'fe_group_id' column
-    df.drop('fe_group_id', axis=1, inplace=True)
 
     return df
